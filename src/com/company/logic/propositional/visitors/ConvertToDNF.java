3f44e60eb2030c4be70e5359c91739f887ee516c
zabiela-artem <37981967+zabiela-artem@users.noreply.github.com>
2018-04-19 14:03:21 +0300

    added wumpus world agents

---
+++ src/com/company/logic/propositional/visitors/ConvertToDNF.java
@@ -0,0 +1,30 @@
+package com.company.logic.propositional.visitors;
+
+import com.company.logic.propositional.parsing.ast.Sentence;
+
+/**
+ * Convert a Sentence into an equivalent Disjunctive Normal Form (DNF) Sentence.
+ * A Sentence is in DNF if it is a disjunction of conjunction of literals.
+ *
+ * @author Ciaran O'Reilly
+ */
+public class ConvertToDNF {
+    /**
+     * Returns the specified sentence in its logically equivalent disjunction
+     * normal form.
+     *
+     * @param s a propositional logic sentence
+     * @return the input sentence converted to it logically equivalent
+     * disjunction normal form.
+     */
+    public static Sentence convert(Sentence s) {
+        Sentence result = null;
+
+        Sentence nnfSentence = ConvertToNNF.convert(s);
+        Sentence dnfSentence = DistributeAndOverOr.distribute(nnfSentence);
+
+        result = dnfSentence;
+
+        return result;
+    }
+}
