71713c3b1dcec7f5cd21158e29d5bcedc1b692cb
zabiela-artem <37981967+zabiela-artem@users.noreply.github.com>
2018-04-19 13:41:13 +0300

    added wumpus world agents

---
+++ src/com/company/logic/interface/CancellableThread.java
@@ -0,0 +1,46 @@
+package com.company.logic.propositional.inference;
+
+/**
+ * Implements a thread with an additional flag indicating cancellation.
+ *
+ * @author Ruediger Lunde
+ * @author Mike Stampone
+ */
+public class CancellableThread extends Thread {
+
+    private volatile boolean isCancelled;
+
+    public CancellableThread() {
+    }
+
+    public CancellableThread(Runnable runnable) {
+        super(runnable);
+    }
+
+    /**
+     * Returns <code>true</code> if the current thread is cancelled
+     *
+     * @return <code>true</code> if the current thread is cancelled
+     */
+    public static boolean currIsCancelled() {
+        if (Thread.currentThread() instanceof CancellableThread)
+            return ((CancellableThread) Thread.currentThread()).isCancelled;
+        return false;
+    }
+
+    /**
+     * Returns <code>true</code> if this thread is cancelled
+     *
+     * @return <code>true</code> if this thread is cancelled
+     */
+    public boolean isCancelled() {
+        return isCancelled;
+    }
+
+    /**
+     * Cancels this thread
+     */
+    public void cancel() {
+        isCancelled = true;
+    }
+}
