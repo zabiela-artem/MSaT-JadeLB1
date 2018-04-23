bc87c96af410288d9e73a0bc8bac4378e5c9ea4b
zabiela-artem <37981967+zabiela-artem@users.noreply.github.com>
2018-04-23 09:21:38 +0300

    added wumpus world agents

---
+++ src/com/company/wumpusworld/search/adversarial/AdversarialSearch.java
@@ -0,0 +1,27 @@
+package com.company.wumpusworld.search.adversarial;
+
+import com.company.wumpusworld.search.framework.Metrics;
+
+/**
+ * Variant of the search interface. Since players can only control the next
+ * move, method <code>makeDecision</code> returns only one action, not a
+ * sequence of actions.
+ *
+ * @param <S> The type used to represent states
+ * @param <A> The type of the actions to be used to navigate through the state space
+ * @author Ruediger Lunde
+ */
+public interface AdversarialSearch<S, A> {
+
+    /**
+     * Returns the action which appears to be the best at the given state.
+     */
+    A makeDecision(S state);
+
+    /**
+     * Returns all the metrics of the search.
+     *
+     * @return all the metrics of the search.
+     */
+    Metrics getMetrics();
+}
