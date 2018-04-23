4d4ef240ba52beaa595256990c5ed48249c24b92
zabiela-artem <37981967+zabiela-artem@users.noreply.github.com>
2018-04-23 09:07:22 +0300

    added wumpus world agents

---
+++ src/com/company/wumpusworld/Speleolog.java
@@ -0,0 +1,14 @@
+package com.company.wumpusworld;
+
+import jade.core.Agent;
+
+public class Speleolog extends Agent {
+    protected AgentPosition currentPosition;
+    protected int t = 0;
+    private WumpusKB kb = null;
+
+    @Override
+    protected void setup() {
+        
+    }
+}
