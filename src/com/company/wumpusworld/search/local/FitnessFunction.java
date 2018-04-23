bec3f360e19061aff3f8f580f6233fb901fd2b65
zabiela-artem <37981967+zabiela-artem@users.noreply.github.com>
2018-04-23 09:27:23 +0300

    added wumpus world agents

---
+++ src/com/company/wumpusworld/search/local/FitnessFunction.java
@@ -0,0 +1,24 @@
+package com.company.wumpusworld.search.local;
+
+/**
+ * Artificial Intelligence A Modern Approach (3rd Edition): page 127.<br>
+ * <br>
+ * Each state is rated by the objective function, or (in Genetic Algorithm
+ * terminology) the fitness function. A fitness function should return higher
+ * values for better states.
+ * <br>
+ * Here, we assume that all values are greater or equal to zero.
+ *
+ * @param <A> the type of the alphabet used in the representation of the
+ *            individuals in the population (this is to provide flexibility in
+ *            terms of how a problem can be encoded).
+ * @author Ciaran O'Reilly
+ */
+public interface FitnessFunction<A> {
+
+    /**
+     * @param individual the individual whose fitness is to be accessed.
+     * @return the individual's fitness value (the higher the better).
+     */
+    double apply(Individual<A> individual);
+}
